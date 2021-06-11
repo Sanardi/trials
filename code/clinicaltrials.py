91ef1d79c4714146f1688df3c818d598c2347006
sanardiazam <rubypromise@gmx.com>
2021-06-11 16:21:08 +0100

    first commit
    not done -- getting recursion error when testing outside jupyter notebook, probably something with regex default change with newer version of pandas?

---
+++ code/clinicaltrials.py
@@ -0,0 +1,51 @@
+#author: Marzia Catherine Azam
+
+import pandas as pd
+import json
+
+
+class ClinicalTrials:
+    """Takes 3 files: usan_stems.csv, drugs.csv and clinical_trials2015.jsonl
+     and performs various aggregations and calculation as well as returns selected json output.
+     Choose method named after task to get the desired output.
+     """
+    def __init__(self, drugs_file="drugs.csv", 
+    trials_file="clinical_trials_2015.jsonl", usan_file="usan_stems.csv"):
+        self.drugs = drugs_file
+        self.trials = trials_file
+        self.usan = usan_file
+
+    def prepare_drugs_file(self):
+        df = pd.read_csv(self.drugs)
+        df['alternatives'] = df['itemLabel'] + "|" + df['altLabel_list'] 
+        df['alternatives'] = df['alternatives'].str.lower().str.split('|')
+        print(df.head())
+        return df
+
+    def prepare_trials_file(self):
+        df = pd.read_json(self.trials, lines=True)
+        print(df.head())
+        return df
+
+    def select_only_drug_trials(self):
+        """I filtered by intervention type drug here I was not 
+        expecting to find matches in the other categories. 
+        This can easily be changed with the following olumn if required."""
+        df = self.prepare_trials_file()
+        df_drug_trials = df[df['intervention_type'] == "Drug"]
+        return df_drug_trials
+
+    def clean_df_trials(self):
+        df_drug_trials = self.select_only_drug_trials()
+        df_drug_trials['intervention_name'] = df_drug_trials['intervention_name'].str.lower()
+        df_drug_trials['matches'] = df_drug_trials['intervention_name']
+        words_to_remove = ["and", "intravenous", "or", "+", "-", "/", r"\(.*\)"]
+        for item in words_to_remove:
+            df_drug_trials['matches'] = df_drug_trials['matches'].str.replace(item,"", regex = True)
+
+        df_drug_trials['matches'] = df_drug_trials['matches'].str.lower().str.split(',')
+        return df_drug_trials
+
+
+if __name__ == "__main__":
+    print("running")
---
+++ code/test/test_clinicaltrials.py
@@ -0,0 +1,59 @@
+import unittest
+from clinicaltrials import ClinicalTrials
+
+class ClinicalTrialsTest(unittest.TestCase):
+    """Test Class for testing Clinical Trials, 
+    currently assumes you take the files that are in the directory by checking first entry.
+    This could be extended to test different files if desired"""
+       
+    def setUp(self) -> None:
+        self.clinicaltrials = ClinicalTrials()
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_prepare_drugs_file(self):
+        """checking file is being loaded an return 1st line"""
+
+        data_expected = 'propylthiouracil'
+        df = self.clinicaltrials.prepare_drugs_file()
+        data_received = df.iloc[0].itemLabel
+        self.assertEqual(data_expected, data_received)
+
+    #@unittest.skip("WIP")
+    def test_prepare_trials_file(self):
+        """checking file is being loaded and returns 1st line"""
+        data_expected = 'NCT00293735'
+        df = self.clinicaltrials.prepare_trials_file()
+        data_received = df.iloc[0].nct_id
+        self.assertEqual(data_expected, data_received)
+
+    def test_clean_df_trials(self):
+        data_expected = 'Drug'
+        df = self.test_clean_df_trials()
+        data_sample = df.intervention_type.sample(n=1).reset_index()
+        data_received = data_sample['intervention_type']
+        self.assertEqual(data_expected, data_received)
+
+    @unittest.skip("WIP")    
+    def test_is_consistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Anna", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+    @unittest.skip("WIP")
+    def test_inconsistent_with_duplicate_entries(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Sue", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+    @unittest.skip("WIP")
+    def test_inconsistent_with_different_prefixes(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Sue", "123")
+        self.assertFalse(self.phonebook.is_consistent())
+    @unittest.skip("WIP")
+    def test_phonebook_adds_names_and_numbers(self):
+        self.phonebook.add("Richard", "09158")
+        self.assertIn("Richard", self.phonebook.get_names())
+        self.assertIn("09158", self.phonebook.get_numbers())
+
+
